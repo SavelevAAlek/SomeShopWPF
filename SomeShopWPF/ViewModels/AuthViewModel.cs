e87449191172ffe1a805c018d36f4e65d91d7683
Alexey Savelyev <savelyev.main@gmail.com>
2024-02-02 21:42:15 +0600

    Тест ресурса для оторбажения точек вместо пароля

--- SomeShopWPF/ViewModels/AuthViewModel.cs
+++ SomeShopWPF/ViewModels/AuthViewModel.cs
@@ -4,5 +4,16 @@ namespace SomeShopWPF.ViewModels
 {
     public class AuthViewModel : DialogViewModel
     {
+        private string _username = "Логин";
+        private string _password = "Пароль";
+
+        public string Username { get => _username; set => Set(ref _username, value); }
+        public string Password { get => _password; set => Set(ref _password, value); }
+
+
+        public AuthViewModel()
+        {
+
+        }
     }
 }
