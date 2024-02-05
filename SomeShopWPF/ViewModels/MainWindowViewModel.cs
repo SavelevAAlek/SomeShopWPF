36bf4e6008eee557ec2aacb75d117a51ea05f6d8
Alexey Savelyev <savelyev.main@gmail.com>
2024-02-06 00:37:50 +0600

    Удаление клиента

--- SomeShopWPF/ViewModels/MainWindowViewModel.cs
+++ SomeShopWPF/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,14 @@
-﻿using SomeShopWPF.Models;
+﻿using SomeShopWPF.Commands;
+using SomeShopWPF.Models;
 using SomeShopWPF.Services;
 using SomeShopWPF.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data.SqlClient;
 using System.IO;
 using System.Windows.Documents;
+using System.Windows.Input;
 
 namespace SomeShopWPF.ViewModels
 {
@@ -13,10 +16,25 @@ namespace SomeShopWPF.ViewModels
     {
         private string _conStr = File.ReadAllText(@"..\..\..\Resources\MSSQLcon_str.txt");
 
+        private Client _selectedClient;
+
         public ObservableCollection<Client> ClientsList { get; set; } = new ObservableCollection<Client>();
+        public Client SelectedClient { get => _selectedClient; set => Set(ref _selectedClient, value); }
+
+        public ICommand DeleteCommand { get; set; }
+
         public MainWindowViewModel()
         {
+            DeleteCommand = new LambdaCommand(OnDeleteCommandExecuted, CanDeleteCommandExecuted);
             SetClientTable();            
+        }
+
+        private bool CanDeleteCommandExecuted() => SelectedClient != null ? true : false;
+
+        private void OnDeleteCommandExecuted(object? obj)
+        {
+            DeleteClient(SelectedClient);
+            OnPropertyChanged(nameof(ClientsList));
         }
 
         private void SetClientTable()
@@ -42,6 +60,22 @@ namespace SomeShopWPF.ViewModels
                 }
             }
 
+        }
+
+        private void DeleteClient(Client selectedClient)
+        {
+            var query = $"DELETE FROM Clients WHERE Email = '{selectedClient.Email}'";
+
+            using (SqlConnection connection = new SqlConnection(_conStr))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.ExecuteNonQuery();
+            }
+
+            ClientsList = new ObservableCollection<Client>();
+            SetClientTable();
         }
     }
 }
